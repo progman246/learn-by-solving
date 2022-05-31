dedb46bcfc92e8e9e8be76beba9dbaf1bd3b0ef6
progman246 <hossein.abdi7@gmail.com>
2022-05-31 19:31:41 +0430

    Add My Solution to Repositry

---
+++ problems/0001-FileSystem/solution/CSharp/progman246/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileSystemProblem.Solution;
+
+Solution solution2 = new Solution();
+
+var path = @"c:\temp\Folder1";
+var FileInfo = solution2.Getfiles(path);
+var GetIndex = solution2.MakeIndex(FileInfo);
+
+//string userInput=string.Empty;
+
+//Console.WriteLine("Please Enter Your Words:");
+
+//while (userInput != "END")
+//{
+//    userInput = Console.ReadLine();
+//    var userInputs = userInput.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+//    if (userInputs.Length > 0)
+//    {
+//        foreach (string item in userInputs)
+//        {
+
+//            try
+//            {
+//                var indexofInput = GetIndex[item];
+//                //var repeatCount = indexofInput.Select(x=>x==userInput).Count();
+//                //Console.WriteLine(repeatCount);
+//                int sentenceCount = solution2.ReadFileBySentence(indexofInput, item);
+//                Console.WriteLine($"File:{string.Join(",", indexofInput)},Sentence:{sentenceCount}");
+
+//            }
+//            catch
+//            {
+//                Console.WriteLine($"Your Word {item} does not exist in documents!!!please Try another word or to Exit type END");
+
+//            }
+
+//        }
+//    }
+//    else
+//    {
+//        Console.WriteLine("Please Enter your words ");
+//    }
+//}
+
+
+
+
+
+
---
+++ problems/0001-FileSystem/solution/CSharp/progman246/Solution.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSystemProblem.Solution
+{
+    public class Solution
+    {
+        public IEnumerable<string> Getfiles(string SourceFolder)
+        {
+            var files = Directory.GetFiles(SourceFolder, "*.txt", SearchOption.AllDirectories);
+            return files;
+        }
+
+        public Dictionary<string, IList<string>> MakeIndex(IEnumerable<string> files)
+        {
+            Dictionary<string, IList<string>> result = new Dictionary<string, IList<string>>();
+            foreach (var item in files)
+            {
+                using (var reader = new StreamReader(item))
+                {
+                    var line = reader.ReadLine();
+                    var words = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var word in words)
+                    {
+                        if (result.ContainsKey(word))
+                        {
+                            var fileIndex = result[word];
+                            fileIndex.Add(item);
+                        }
+                        else
+                        {
+                            List<string> fileReffrence = new List<string>();
+                            fileReffrence.Add(item);
+                            result.Add(word, fileReffrence);
+
+                        }
+                    }
+
+                }
+            }
+            return result;
+        }
+
+    //    public int ReadFileBySentence(IEnumerable<string> files,string word)
+    //    {
+           
+    //        int wordCounter = 0;
+    //        foreach (var item in files)
+    //        {
+    //            using (var reader = new StreamReader(item))
+    //            {
+                    
+    //                var line = reader.ReadLine();
+    //                var Sentences = line.Split(".", StringSplitOptions.RemoveEmptyEntries);
+    //                foreach (var sentence in Sentences)
+    //                {
+    //                    if (sentence.Contains(word))
+    //                    {
+    //                        wordCounter++;
+    //                    }
+    //                }
+
+    //            }
+    //        }
+    //        return wordCounter;
+    //    }
+
+
+    }
+}
